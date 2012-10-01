7a6efe64c7660b450214e5ed65a42c959e4c8787
Kamil Páral <kamil.paral@gmail.com>
2012-10-01 23:34:05 +0200

    create config file automatically if needed
    improve error reporting, tag as version 2

--- sendKindle.py
+++ sendKindle.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 '''
 CLI tool for sending files via email to your Kindle device.
-Program requires configuration file. Run the program to see the example file.
+You need to have a working IMAP account.
 
 This script is originally based on:
-http://rakesh.fedorapeople.org/misc/pykindle.py
+http://rakesh.fedorapeople.org/misc/pykindle.py (Public Domain)
 
 Author: Kamil Páral <kamil.paral /at/ gmail.com>
 '''
@@ -27,6 +27,7 @@ import optparse
 import os
 import smtplib
 import sys
+import traceback
 from StringIO import StringIO
 from email import encoders
 from email.MIMEBase import MIMEBase
@@ -34,31 +35,77 @@ from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 from email.generator import Generator
 
-_version = '1'
+_version = '2'
 
 
 class SendKindle:
-    conffile='~/.config/sendkindle/sendkindle.cfg'
-    sample_conf='''  [Default]
-  smtp_server = smtp.gmail.com
-  smtp_port = 465
-  smtp_login = username
-  user_email = username@gmail.com
-  kindle_email = username@free.kindle.com
-  # optional
-  smtp_password = password
-  convert = False'''
+    smtp_server = None
+    smtp_port = 0
+    smtp_login = None
+    user_email = None
+    kindle_email = None
     smtp_password = None
+    convert = False
+
+    parser = None
+    files = []
+
+    conffile = os.path.expanduser('~/.config/sendKindle/sendKindle.cfg')
+    sample_conf = '''[Default]
+smtp_server = smtp.gmail.com
+smtp_port = 465
+smtp_login = username
+user_email = username@gmail.com
+kindle_email = username@free.kindle.com
+# optional
+smtp_password = password
+convert = False'''
+
 
     def __init__(self):
         self.parse_args()
         self.read_config()
         self.check_args()
 
+
+    def create_config(self):
+        '''Creates an empty config file from the template if no config file
+        exists. Halts program execution in that case. Otherwise nothing
+        happens.'''
+
+        try:
+            if not os.path.exists(self.conffile):
+                # create directory
+                parent = os.path.dirname(self.conffile)
+                if not os.path.exists(parent):
+                    os.makedirs(parent)
+                # create file
+                f = open(self.conffile, 'w')
+                f.write(self.sample_conf)
+                f.close()
+            else:
+                return
+        except IOError:
+            traceback.print_exc()
+            message = ('Error creating a new config file, maybe wrong '
+                       'permissions?')
+            print >> sys.stderr, message
+            sys.exit(4)
+        else:
+            message = ('A new config file created in: %s\n'
+                'Please edit it, provide correct values and run the program '
+                'again.' % self.conffile)
+            print >> sys.stderr, message
+            sys.exit(5)
+
+
     def read_config(self):
+        '''Parse the config file. Create a new one if needed.'''
+
+        self.create_config()
         config = ConfigParser.SafeConfigParser()
         try:
-            if not config.read([os.path.expanduser(self.conffile)]):
+            if not config.read([self.conffile]):
                 raise IOError('%s could not be read' % self.conffile)
             self.smtp_server = config.get('Default', 'smtp_server')
             self.smtp_port = config.getint('Default', 'smtp_port')
@@ -71,28 +118,29 @@ class SendKindle:
                                       config.get('Default', 'smtp_password'))
             if config.has_option('Default', 'convert'):
                 # prefer value from cmdline option
-                self.convert = self.convert or config.getboolean('Default', 'convert')
-        except (IOError, ConfigParser.Error, ValueError) as e:
-            print e
-            message = "You're missing a program configuration file. Please " \
-                      "create %s with the following example content:\n" \
-                      "%s" % (self.conffile, self.sample_conf)
+                self.convert = (self.convert or
+                                config.getboolean('Default', 'convert'))
+        except (IOError, ConfigParser.Error, ValueError):
+            traceback.print_exc()
+            message = ("Your config file could not be read or contains "
+                       "invalid values: %s" % self.conffile)
             print >> sys.stderr, message
             sys.exit(3)
 
+
     def parse_args(self):
-        usage = 'Usage: %prog [options] FILE...\n' \
-                '  FILE is a file to be sent as an email attachment.'
-        description = 'SendKindle will send any number of files via email ' \
-                      'to your Kindle device.'
+        usage = ('Usage: %prog [options] FILE...\n'
+                '  FILE is a file to be sent as an email attachment.')
+        description = ('SendKindle will send any number of files via email '
+                      'to your Kindle device.')
         parser = optparse.OptionParser(usage=usage, version=_version,
                                        description=description)
         self.parser = parser
-        parser.add_option('--password', help='Use provided password instead ' \
-            'of smtp_password value from your config file')
-        parser.add_option('-c', '--convert', action='store_true', default=False,
-            help='Ask Kindle service to convert the documents to Kindle format' \
-                 ' (mainly for PDFs) [default: %default]')
+        parser.add_option('--password', help=('Use provided password instead '
+            'of smtp_password value from your config file'))
+        parser.add_option('-c', '--convert', action='store_true',
+            default=False, help=('Ask Kindle service to convert the documents '
+            'to Kindle format (mainly for PDFs) [default: %default]'))
         (options, args) = parser.parse_args()
 
         self.convert = options.convert
@@ -102,9 +150,12 @@ class SendKindle:
             parser.error('No files provided as arguments! See --help.')
         self.files = args
 
+
     def check_args(self):
         if not self.smtp_password:
-            self.parser.error('Missing option (config file or command line): smtp_password')
+            self.parser.error('Missing option (config file or command line): '
+                              'smtp_password')
+
 
     def send_mail(self):
         '''Send email with attachments'''
@@ -128,25 +179,42 @@ class SendKindle:
         msg = fp.getvalue()
 
         # send email
-        mailServer = smtplib.SMTP_SSL(host=self.smtp_server, port=self.smtp_port)
-        mailServer.login(self.smtp_login, self.smtp_password)
-        mailServer.sendmail(self.user_email, self.kindle_email, msg)
-        mailServer.close()
+        try:
+            mail_server = smtplib.SMTP_SSL(host=self.smtp_server,
+                                          port=self.smtp_port)
+            mail_server.login(self.smtp_login, self.smtp_password)
+            mail_server.sendmail(self.user_email, self.kindle_email, msg)
+            mail_server.close()
+        except smtplib.SMTPException:
+            traceback.print_exc()
+            message = ('Communication with your SMTP server failed. Maybe '
+                       'wrong connection details? Check exception details and '
+                       'your config file: %s' % self.conffile)
+            print >> sys.stderr, message
+            sys.exit(7)
 
         print('Sent email to %s' % self.kindle_email)
+
 
     def get_attachment(self, file_path):
         '''Get file as MIMEBase message'''
 
-        file_ = open(file_path, 'rb')
-        attachment = MIMEBase('application', 'octet-stream')
-        attachment.set_payload(file_.read())
-        file_.close()
-        encoders.encode_base64(attachment)
+        try:
+            file_ = open(file_path, 'rb')
+            attachment = MIMEBase('application', 'octet-stream')
+            attachment.set_payload(file_.read())
+            file_.close()
+            encoders.encode_base64(attachment)
 
-        attachment.add_header('Content-Disposition', 'attachment',
-                              filename=os.path.basename(file_path))
-        return attachment
+            attachment.add_header('Content-Disposition', 'attachment',
+                                  filename=os.path.basename(file_path))
+            return attachment
+        except IOError:
+            traceback.print_exc()
+            message = ('The requested file could not be read. Maybe wrong '
+                       'permissions?')
+            print >> sys.stderr, message
+            sys.exit(6)
 
 
 def main():
@@ -154,10 +222,10 @@ def main():
     try:
         kindle = SendKindle()
         kindle.send_mail()
-    except KeyboardInterrupt as e:
+    except KeyboardInterrupt:
         print >> sys.stderr, 'Program interrupted, exiting...'
         sys.exit(10)
 
-if __name__ == '__main__':
-        main()
 
+if __name__ == '__main__':
+    main()
